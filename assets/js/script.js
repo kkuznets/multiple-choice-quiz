953a6383a69a001eec8452dcf42fb58e295856ed
Kirill Kuznetsov <yours.kkuznets@gmail.com>
2020-03-18 12:57:45 +1100

    Updated quiz logic
    Added functions to show questions and check user choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,7 +68,40 @@ function buildQuiz() {
     quizContainer.innerHTML = output.join('');
 }
 
-function showResults() {}
+function showResults() {
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        // if answer is correct
+        if (userAnswer === currentQuestion.correctAnswer) {
+            // add to the number of correct answers
+            numCorrect++;
+
+            // color the answers green
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else {
+            // color the answers red
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
 
 // display quiz right away
 buildQuiz();
